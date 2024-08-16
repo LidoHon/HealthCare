9807525d65e95ccbac1509e4378ab4e0d5ba4a12
LidoHon <lidohon10@gmail.com>
2024-08-16 11:23:54 +0300

    otp verfication for admin part is finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
+import PasskeyModel from "@/components/PasskeyModel";
 import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({searchParams}:SearchParamProps) {
+
+  const isAdmin = searchParams.admin === 'true'
   return (
     <div className="flex h-screen max-h-screen">
       {/* todo: will make otp verification or passkey model soon */}
+      {isAdmin && <PasskeyModel/>}
+
+
       <section className="remove-scrollbar container">
         <div className="sub-container  max-w-[496px]">
           <div className="flex flex-row gap-2">
